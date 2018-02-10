c43edf5e9a17addb3f01eeceeecd226aa5c052b0
Sogues <mail.caoke@icloud.com>
2018-02-10 18:40:53 +0800

    complete softmax function

--- ERROR.md
+++ ERROR.md
@@ -4,3 +4,9 @@
     len(a) = len(b) 且 
         np.maximum(a) < array.shape[0]
         np.maximum(b) < array.shape[1]
+
+## numpy array
+    a.shape (10,)   b.shape (10, 1)
+    (a/b).shape (10, 10)
+    c.shape (10,)
+    (a/c).shpae (10,)
--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -1,6 +1,7 @@
 import numpy as np
 from random import shuffle
 from past.builtins import xrange
+from tqdm import tqdm
 
 def softmax_loss_naive(W, X, y, reg):
   """
@@ -32,12 +33,24 @@ def softmax_loss_naive(W, X, y, reg):
   #############################################################################
   train_num = X.shape[0]
   class_num = W.shape[1]
-  scores = X.dot(W)
-  scores -= np.max(scores, axis=1, keepdims=True)
-  prop = np.exp(scores[range(train_num), list(y)]) / np.sum(np.exp(scores), axis=1)
-  loss = -np.sum(np.log(prop)) / train_num + reg * np.sum(W*W)
 
-  dW += reg * 2 * W
+  for i in tqdm(xrange(train_num)):
+      scores = X[i].dot(W)
+      shift_scores = scores - np.max(scores)
+      shift_scores_sum = np.sum(np.exp(shift_scores))
+      loss_i = - shift_scores[y[i]] + np.log(shift_scores_sum)
+      loss += loss_i
+      for j in xrange(class_num):
+          softmax_output = np.exp(shift_scores[j]) / shift_scores_sum
+          if j == y[i]:
+              dW[:, j] += (-1 + softmax_output) * X[i]
+          else:
+              dW[:, j] += softmax_output * X[i]
+
+  loss = loss / train_num + reg * np.sum(W*W)
+  dW = dW / train_num + 2 * reg * W
+
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -61,7 +74,30 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+
+  train_num = X.shape[0]
+  feature_num = X.shape[1]
+  class_num = W.shape[1]
+
+
+  scores = X.dot(W)
+  shift_scores = scores - np.max(scores, axis=1, keepdims=True)
+
+  shift_scores_sum = np.sum(np.exp(shift_scores), axis=1, keepdims=True)
+  loss_x = (
+          - shift_scores[range(train_num), list(y)] +
+          np.log(shift_scores_sum)
+          )
+
+  loss = np.sum(loss_x) / train_num + reg * np.sum(W*W)
+
+  softmax_output = (np.exp(shift_scores) / shift_scores_sum)
+  softmax_output[range(train_num), list(y)] -= 1
+  dW = X.T.dot(softmax_output)
+
+  loss = loss / train_num + reg * np.sum(W*W)
+  dW = dW / train_num + 2 * reg * W
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
